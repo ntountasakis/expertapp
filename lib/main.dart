93510e2f71fc377b705b31ebc521750b36270866
Nick Tountasakis <ntountas@Nicks-MacBook-Air.local>
2021-10-27 19:43:12 -0500

    iOS building with Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:expertapp/src/expert_listings.dart';
-import 'package:expertapp/src/expert_profile_page.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
