edb484b14cf65a24323b78f0264c180e742533ed
Nick Tountasakis <ntountas@Nicks-MacBook-Air.local>
2022-01-09 20:07:56 -0600

    Implement Facebook Sign In

--- lib/src/screens/sign_in.dart
+++ lib/src/screens/sign_in.dart
@@ -1,6 +1,8 @@
 import 'dart:developer';
 
+import 'package:expertapp/src/auth/facebook_sign_in.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
 import 'package:flutter_signin_button/flutter_signin_button.dart';
 
 class SignIn extends StatefulWidget {
@@ -19,6 +21,7 @@ class _SignIn extends State<SignIn> {
             Buttons.Facebook,
             onPressed: () async {
               log('Facebook sign in pressed');
+              await FacebookSignIn.signInWithFacebook();
             },
           )
         ])));
