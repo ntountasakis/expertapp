4d8f8f79cefd6938b99d271f5fc3cd8be1120b53
Nick Tountasakis <ntountas@Nicks-MacBook-Air.local>
2022-02-15 22:37:45 -0600

    Configure firebase emulators

--- lib/src/firebase/emulator/configure_emulator.dart
+++ lib/src/firebase/emulator/configure_emulator.dart
@@ -2,10 +2,11 @@ import 'dart:io';
 
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_database/firebase_database.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 
 Future connectToFirebaseEmulator() async {
   final localhostString = Platform.isAndroid ? '10.0.2.2' : 'localhost';
-  FirebaseDatabase.instance.setPersistenceEnabled(false);
-  FirebaseDatabase.instance.useDatabaseEmulator(localhostString, 9000);
-  await FirebaseAuth.instance.useAuthEmulator(localhostString, 9099);
+  FirebaseDatabase.instance.useDatabaseEmulator(localhostString, 9003);
+  await FirebaseStorage.instance.useStorageEmulator(localhostString, 9006);
+  await FirebaseAuth.instance.useAuthEmulator(localhostString, 9000);
 }
