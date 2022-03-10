fb26b5958f6315d015965178cd6f6cbd95c677a8
Nick Tountasakis <ntountas@Nicks-MacBook-Air.local>
2022-03-09 21:08:53 -0600

    Agora wip, can't test using ios simulator! Going to try to get app to run on Android

---
+++ lib/src/agora/agora_app_id.dart
@@ -0,0 +1,5 @@
+class AgoraAppId {
+  static final String ID = '5dafa6bf2cbc4fcea4e4004adeea4485';
+  static final String TEST_TOKEN =
+      '0065dafa6bf2cbc4fcea4e4004adeea4485IABHzAvp9RIOeubpbOGF/bqd/J6IPnqDNZdox0oFInlLYgx+f9gAAAAAEABgTjz6H7sqYgEAAQAeuypi';
+}
