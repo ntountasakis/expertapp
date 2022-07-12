a4bd1b63003e7d10b1d27df771eb4a13c0b94036
Nicholas Tountasakis <nicholas.tountasakis@gmail.com>
2022-07-11 20:29:50 -0500

    Revived chat room in call transaction page!

---
+++ lib/src/call_server/widgets/call_server_editable_chat_button.dart
@@ -0,0 +1,25 @@
+import 'package:expertapp/src/firebase/cloud_functions/callable_api.dart';
+import 'package:expertapp/src/firebase/firestore/document_models/document_wrapper.dart';
+import 'package:expertapp/src/firebase/firestore/document_models/user_metadata.dart';
+import 'package:expertapp/src/screens/chat_page.dart';
+import 'package:flutter/material.dart';
+
+Widget buildEditableChatButton({required BuildContext context, required String currentUserId, 
+required DocumentWrapper<UserMetadata> calledUserMetadata}) {
+  final ButtonStyle style =
+      ElevatedButton.styleFrom(textStyle: const TextStyle(fontSize: 20));
+  return ElevatedButton(
+      style: style,
+      onPressed: () async {
+        final chatroomId =
+            await lookupChatroomId(calledUserMetadata.documentId);
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => ChatPage(
+                currentUserId, calledUserMetadata, chatroomId),
+          ),
+        );
+      },
+      child: const Text('Chat Expert'));
+}
