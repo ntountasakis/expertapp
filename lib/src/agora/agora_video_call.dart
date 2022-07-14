7c1a73babf54b72ac9cff8a33564b2abfe1385d5
Nicholas Tountasakis <nicholas.tountasakis@gmail.com>
2022-07-13 21:50:18 -0500

    Simple video call buttons agora

--- lib/src/agora/agora_video_call.dart
+++ lib/src/agora/agora_video_call.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:developer';
 
 import 'package:agora_rtc_engine/rtc_engine.dart';
 import 'package:agora_rtc_engine/rtc_local_view.dart' as RtcLocalView;
@@ -12,17 +13,25 @@ class AgoraVideoCall extends StatefulWidget {
   final String agoraToken;
   final int agoraUid;
 
-  AgoraVideoCall({required this.agoraChannelName, required this.agoraToken,
-  required this.agoraUid});
+  AgoraVideoCall(
+      {required this.agoraChannelName,
+      required this.agoraToken,
+      required this.agoraUid});
 
   @override
   _AgoraVideoCallState createState() => _AgoraVideoCallState();
+}
+
+class CallButtonState {
+  bool videoEnabled = true;
+  bool audioEnabled = true;
 }
 
 class _AgoraVideoCallState extends State<AgoraVideoCall> {
   int? _remoteUid;
   bool _localUserJoined = false;
   late RtcEngine _engine;
+  final buttonState = CallButtonState();
 
   @override
   void initState() {
@@ -40,19 +49,19 @@ class _AgoraVideoCallState extends State<AgoraVideoCall> {
     _engine.setEventHandler(
       RtcEngineEventHandler(
         joinChannelSuccess: (String channel, int uid, int elapsed) {
-          print("local user $uid joined");
+          log("local user $uid joined");
           setState(() {
             _localUserJoined = true;
           });
         },
         userJoined: (int uid, int elapsed) {
-          print("remote user $uid joined");
+          log("remote user $uid joined");
           setState(() {
             _remoteUid = uid;
           });
         },
         userOffline: (int uid, UserOfflineReason reason) {
-          print("remote user $uid left channel");
+          log("remote user $uid left channel");
           setState(() {
             _remoteUid = null;
           });
@@ -64,31 +73,101 @@ class _AgoraVideoCallState extends State<AgoraVideoCall> {
         widget.agoraToken, widget.agoraChannelName, null, widget.agoraUid);
   }
 
+  Widget buildCameraButton() {
+    final MaterialColor cameraColor =
+        buttonState.videoEnabled ? Colors.green : Colors.red;
+    return IconButton(
+      icon: Icon(
+        Icons.camera_alt,
+        size: 20,
+        color: cameraColor,
+      ),
+      onPressed: () async {
+        await _engine.muteLocalVideoStream(!buttonState.videoEnabled);
+        setState(() {
+          buttonState.videoEnabled = !buttonState.videoEnabled;
+        });
+      },
+    );
+  }
+
+  Widget buildMicButton() {
+    final MaterialColor micColor =
+        buttonState.audioEnabled ? Colors.green : Colors.red;
+    return IconButton(
+      icon: Icon(
+        Icons.mic,
+        size: 20,
+        color: micColor,
+      ),
+      onPressed: () async {
+        await _engine.muteLocalAudioStream(!buttonState.audioEnabled);
+        setState(() {
+          buttonState.audioEnabled = !buttonState.audioEnabled;
+        });
+      },
+    );
+  }
+
+  Widget buildEndCallButton(BuildContext) {
+    return IconButton(
+      icon: Icon(
+        Icons.call_end_rounded,
+        size: 20,
+        color: Colors.red,
+      ),
+      onPressed: () async {
+        await _engine.destroy();
+        Navigator.pop(context);
+      },
+    );
+  }
+
+  Widget buildVideoButtons(BuildContext context) {
+    return Row(
+      children: [
+        buildCameraButton(),
+        SizedBox(width: 20),
+        buildMicButton(),
+        SizedBox(width: 20),
+        buildEndCallButton(context)
+      ],
+    );
+  }
+
   // Create UI with local view and remote view
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Agora Video Call'),
-      ),
-      body: Stack(
-        children: [
-          Center(
-            child: _remoteVideo(),
-          ),
-          Align(
-            alignment: Alignment.topLeft,
-            child: Container(
-              width: 100,
-              height: 150,
-              child: Center(
-                child: _localUserJoined
-                    ? RtcLocalView.SurfaceView()
-                    : CircularProgressIndicator(),
+    return WillPopScope(
+      onWillPop: () async => false,
+      child: Scaffold(
+        appBar: AppBar(
+          automaticallyImplyLeading: false,
+          title: const Text('Video Call'),
+        ),
+        body: Stack(
+          children: [
+            Center(
+              child: _remoteVideo(),
+            ),
+            Align(
+              alignment: Alignment.topLeft,
+              child: Container(
+                width: 100,
+                height: 150,
+                child: Center(
+                  child: _localUserJoined
+                      ? RtcLocalView.SurfaceView()
+                      : CircularProgressIndicator(),
+                ),
               ),
             ),
-          ),
-        ],
+            Positioned(
+              bottom: 30,
+              child: buildVideoButtons(context),
+            )
+          ],
+        ),
       ),
     );
   }
