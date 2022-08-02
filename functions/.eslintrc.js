17bb2ae17e46d2882db2a9c2e43e01267f94a3c9
Nicholas Tountasakis <nicholas.tountasakis@gmail.com>
2022-08-02 07:34:25 -0500

    Stripe customer id created on user signup

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -28,5 +28,6 @@ module.exports = {
   rules: {
     "quotes": ["error", "double"],
     "import/no-unresolved": 0,
+    "max-len": ["error", {"code": 120}],
   },
 };
--- functions/src/cloud_functions/user_signup.ts
+++ functions/src/cloud_functions/user_signup.ts
@@ -1,5 +1,6 @@
 import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
+import Stripe from "stripe";
 
 export const userSignup = functions.https.onCall(async (data, context) => {
   if (context.auth == null) {
@@ -24,9 +25,29 @@ export const userSignup = functions.https.onCall(async (data, context) => {
 
   const batch = admin.firestore().batch();
 
+  // eslint-disable-next-line max-len
+  const stripe = new Stripe("sk_test_51LLQIdAoQ8pfRhfFWhXXPMmQkBMR1wAZSiFAc0fRZ3OQfnVJ3Mo5MXt65rv33lt0A7mzUIRWahIbSt2iFDFDrZ6C00jF2hT9eZ", {
+    apiVersion: "2020-08-27",
+  });
+
+  let stripeCustomerId = "";
+  try {
+    const stripeCustomerResponse = await stripe.customers.create();
+    stripeCustomerId = stripeCustomerResponse.id;
+  } catch (error) {
+    if (error instanceof stripe.errors.StripeAPIError) {
+      throw new Error(`Cannot create Stripe customer. Api Error: ${error.message}`);
+    } else if (error instanceof stripe.errors.StripeInvalidRequestError) {
+      throw new Error(`Cannot create Stripe customer.  Invalid Request Error: ${error.message}`);
+    } else {
+      throw new Error(`Cannot create Stripe customer.  Unknown Error: ${error}`);
+    }
+  }
+
   const firebaseUser = {
     "firstName": firstName,
     "lastName": lastName,
+    "stripe_customer_id": stripeCustomerId,
   };
   batch.set(admin.firestore().collection("users").doc(uid), firebaseUser);
 
