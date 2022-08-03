5845cd57d71662843d3cd99185064f27ddc10b8b
Nicholas Tountasakis <nicholas.tountasakis@gmail.com>
2022-08-02 22:17:07 -0500

    Started payment intent create function with simple test

---
+++ server/src/stripe/constants.ts
@@ -0,0 +1,3 @@
+export default class StripeConstants {
+    static MAX_PAYMENT_INTENT_DESCRIPTION_LENGTH = 25
+}
---
+++ server/src/stripe/payment_intent_creator.ts
@@ -0,0 +1,44 @@
+import Stripe from "stripe";
+import StripeConstants from "./constants";
+
+export default async function createStripePaymentIntent(customerId: string, customerEmail: string,
+    amountToBillInCents: number, paymentDescription: string):
+    Promise<[valid: boolean, errorMessage: string, clientSecret: string]> {
+  // eslint-disable-next-line max-len
+  const stripe = new Stripe("sk_test_51LLQIdAoQ8pfRhfFWhXXPMmQkBMR1wAZSiFAc0fRZ3OQfnVJ3Mo5MXt65rv33lt0A7mzUIRWahIbSt2iFDFDrZ6C00jF2hT9eZ", {
+    apiVersion: "2020-08-27",
+  });
+
+  let errorMessage = `PaymentIntent create fail for customerId: ${customerId} \n`;
+
+  if (paymentDescription.length > StripeConstants.MAX_PAYMENT_INTENT_DESCRIPTION_LENGTH) {
+    errorMessage += `Payment descripton too long. Length :${paymentDescription.length} > 
+        Max length: ${StripeConstants.MAX_PAYMENT_INTENT_DESCRIPTION_LENGTH}`;
+    return [false, errorMessage, ""];
+  }
+  try {
+    const paymentIntentResponse = await stripe.paymentIntents.create({
+      customer: customerId,
+      amount: amountToBillInCents,
+      currency: "usd",
+      payment_method_types: ["card"],
+      receipt_email: customerEmail,
+      statement_descriptor: "Call Transaction Initiate",
+    });
+    if (paymentIntentResponse.client_secret != null) {
+      return [true, "", paymentIntentResponse.client_secret];
+    }
+    errorMessage += "Null client_secret";
+  } catch (error) {
+    if (error instanceof stripe.errors.StripeAPIError) {
+      errorMessage += `Api Error. Code: ${error.code} Message: ${error.message} Param: ${error.param}`;
+    } else if (error instanceof stripe.errors.StripeInvalidRequestError) {
+      errorMessage += `Invalid Request Error. Code: ${error.code} Message: ${error.message} Param: ${error.param}`;
+    } else if (error instanceof stripe.errors.StripeCardError) {
+      errorMessage += `Card Error. Code: ${error.code} Message: ${error.message} Param: ${error.param}`;
+    } else {
+      errorMessage += `Unhandled error type: ${error}`;
+    }
+  }
+  return [false, errorMessage, ""];
+}
