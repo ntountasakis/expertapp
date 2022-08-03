89fd2f2403db8726356fdcbd015e1bf8c1f1c492
Nicholas Tountasakis <nicholas.tountasakis@gmail.com>
2022-08-02 22:17:07 -0500

    Started payment intent create function with simple test

--- server/test/stripe.test.ts
+++ server/test/stripe.test.ts
@@ -1,28 +1,11 @@
-import Stripe from "stripe";
-// eslint-disable-next-line max-len
-const stripe = new Stripe("sk_test_51LLQIdAoQ8pfRhfFWhXXPMmQkBMR1wAZSiFAc0fRZ3OQfnVJ3Mo5MXt65rv33lt0A7mzUIRWahIbSt2iFDFDrZ6C00jF2hT9eZ", {
-  apiVersion: "2020-08-27",
-});
+import createStripePaymentIntent from "../src/stripe/payment_intent_creator";
 
-test("create customers", () => {
-  stripe.customers.create({
-    email: "customer@example.com",
-  })
-      .then((customer) => console.log(customer.id))
-      .catch((error) => console.error(error));
-});
-
-test("setup intent for customer", async () => {
+test("PaymentIntent should fail: Description too long", async () => {
   const customerId = "cus_M5nT1n5pP6F4aA"; // jan@example.com
-  const paymentMethodId = "foo";
-  const setupIntent = await stripe.setupIntents.create({
-    payment_method_types: ["card"],
-    confirm: true,
-    customer: customerId,
-    usage: "off_session",
-    flow_directions: ["inbound"],
-    payment_method: paymentMethodId,
-  });
-
-  console.log(setupIntent);
+  const customerEmail = "jan@example.com";
+  const [valid, errorMessage, clientSecret] =
+    await createStripePaymentIntent(customerId, customerEmail, 2000, "Call Transaction Initiate");
+  expect(valid).toBe(false);
+  expect(errorMessage).not.toBe("");
+  expect(clientSecret).toBe("");
 });
