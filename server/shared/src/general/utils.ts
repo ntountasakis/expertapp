1fec5f36485dd6768864a0e5b5a89bc7cc896004
Nicholas Tountasakis <nicholas.tountasakis@gmail.com>
2023-01-03 20:38:22 -0600

    Simplify usage of getting ms since epoch.
    Looks like there's a bug in the emulators, ms since
    epoch is returning adjusted for the timezone which
    it shouldnt.
    
    See this line: https://github.com/flutter/flutter/issues/103908

--- server/shared/src/general/utils.ts
+++ server/shared/src/general/utils.ts
@@ -3,9 +3,5 @@ export function isStringDefined(aString: string): boolean {
 }
 
 export function getUtcMsSinceEpoch(): number {
-    var date = new Date();
-    var now_utc = Date.UTC(date.getUTCFullYear(), date.getUTCMonth(),
-        date.getUTCDate(), date.getUTCHours(),
-        date.getUTCMinutes(), date.getUTCSeconds());
-    return now_utc;
+    return new Date().getTime();
 }
