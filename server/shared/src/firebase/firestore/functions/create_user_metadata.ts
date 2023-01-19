3c3bc59da52ea818feb688d089db6b76617b7eb2
Nicholas Tountasakis <nicholas.tountasakis@gmail.com>
2023-01-18 20:54:45 -0600

    Fix default empty description

--- server/shared/src/firebase/firestore/functions/create_user_metadata.ts
+++ server/shared/src/firebase/firestore/functions/create_user_metadata.ts
@@ -1,15 +1,18 @@
 import { getUserMetadataDocumentRef } from "../document_fetchers/fetchers";
 import { UserMetadata } from "../models/user_metadata";
 
-export function createUserMetadata({batch, uid, firstName, lastName, profilePicUrl}:
-    {batch : FirebaseFirestore.WriteBatch, uid: string, firstName: string, lastName: string,
-      profilePicUrl: string}): void {
+export function createUserMetadata({ batch, uid, firstName, lastName, profilePicUrl }:
+  {
+    batch: FirebaseFirestore.WriteBatch, uid: string, firstName: string, lastName: string,
+    profilePicUrl: string
+  }): void {
   const firebaseUserMetadata: UserMetadata = {
     "firstName": firstName,
     "lastName": lastName,
+    "description": "",
     "profilePicUrl": profilePicUrl,
     "runningSumReviewRatings": 0,
     "numReviews": 0,
   };
-  batch.set(getUserMetadataDocumentRef({uid: uid}), firebaseUserMetadata);
+  batch.set(getUserMetadataDocumentRef({ uid: uid }), firebaseUserMetadata);
 }
