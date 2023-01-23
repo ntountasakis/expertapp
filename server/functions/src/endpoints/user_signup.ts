e5c14474706bf6d85e05ebcc13258e1e4dc4477f
Nicholas Tountasakis <nicholas.tountasakis@gmail.com>
2023-01-22 19:23:16 -0600

    Switch to use transaction vs batch to commonize approach

--- server/functions/src/endpoints/user_signup.ts
+++ server/functions/src/endpoints/user_signup.ts
@@ -1,5 +1,4 @@
 import * as functions from "firebase-functions";
-import * as admin from "firebase-admin";
 import {getDefaultProfilePicUrl} from "../../../shared/src/firebase/storage/functions/get_default_profile_pic_url";
 import {StripeProvider} from "../../../shared/src/stripe/stripe_provider";
 import {createStripeCustomer} from "../../../shared/src/stripe/util";
@@ -18,10 +17,8 @@ export const userSignup = functions.https.onCall(async (data, context) => {
   const stripeCustomerId = await createStripeCustomer({stripe: StripeProvider.STRIPE});
   const profilePicUrl = "profilePicUrl" in data ? data.profilePicUrl : getDefaultProfilePicUrl();
 
-  const batch = admin.firestore().batch();
-  createExpertUser({batch: batch, uid: uid, firstName: firstName, lastName: lastName, email: email,
+  await createExpertUser({uid: uid, firstName: firstName, lastName: lastName, email: email,
     stripeCustomerId: stripeCustomerId, profilePicUrl: profilePicUrl});
-  await batch.commit();
 
   console.log(`User ${uid} signed up!`);
 });
