2f3cf6c59eb64149339d291eedcd45bdf00d6b20
Nicholas Tountasakis <nicholas.tountasakis@gmail.com>
2023-01-22 21:22:53 -0600

    Fixup storage buckets. The folder name isnt apart of the bucket name. Fixes the local dev issue where it wasnt upload to the profilePics/ folder we expected.

--- server/shared/src/firebase/storage/functions/get_default_profile_pic_url.ts
+++ server/shared/src/firebase/storage/functions/get_default_profile_pic_url.ts
@@ -2,10 +2,10 @@ import * as admin from "firebase-admin";
 import { StoragePaths } from "../storage_paths";
 export function getDefaultProfilePicUrl(): string {
   const defaultProfilePicName = "Portrait_Placeholder.png";
-  const pictureBucket = StoragePaths.STORAGE_URL + StoragePaths.PROFILE_PIC_BUCKET;
+  const pictureBucket = StoragePaths.BUCKET_URL + StoragePaths.PROFILE_PIC_FOLDER;
   const defaultPicFile = admin.storage()
-      .bucket(pictureBucket)
-      .file(defaultProfilePicName);
+    .bucket(pictureBucket)
+    .file(defaultProfilePicName);
 
   return defaultPicFile.publicUrl();
 }
