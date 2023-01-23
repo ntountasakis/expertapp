877ce2a774248c921b2887abf75cb1d530f46b16
Nicholas Tountasakis <nicholas.tountasakis@gmail.com>
2023-01-22 19:23:16 -0600

    Switch to use transaction vs batch to commonize approach

--- server/shared/src/firebase/firestore/functions/create_expert_user.ts
+++ server/shared/src/firebase/firestore/functions/create_expert_user.ts
@@ -1,12 +1,13 @@
+import * as admin from "firebase-admin";
 import { getPrivateUserDocumentRef, getPublicExpertInfoDocumentRef } from "../document_fetchers/fetchers";
 import { PrivateUserInfo } from "../models/private_user_info";
 import { PublicExpertInfo } from "../models/public_expert_info";
 
-export function createExpertUser({ batch, uid, firstName, lastName, email, profilePicUrl, stripeCustomerId }:
+export async function createExpertUser({ uid, firstName, lastName, email, profilePicUrl, stripeCustomerId }:
   {
-    batch: FirebaseFirestore.WriteBatch, uid: string, firstName: string, lastName: string, email: string,
+    uid: string, firstName: string, lastName: string, email: string,
     profilePicUrl: string, stripeCustomerId: string
-  }): void {
+  }): Promise<void> {
   const privateUserInfo: PrivateUserInfo = {
     "email": email,
     "stripeCustomerId": stripeCustomerId,
@@ -20,6 +21,8 @@ export function createExpertUser({ batch, uid, firstName, lastName, email, profi
     "runningSumReviewRatings": 0,
     "numReviews": 0,
   };
-  batch.set(getPrivateUserDocumentRef({ uid: uid }), privateUserInfo);
-  batch.set(getPublicExpertInfoDocumentRef({ uid: uid }), publicExpertInfo);
+  await admin.firestore().runTransaction(async (transaction) => {
+    transaction.set(getPrivateUserDocumentRef({ uid: uid }), privateUserInfo);
+    transaction.set(getPublicExpertInfoDocumentRef({ uid: uid }), publicExpertInfo);
+  });
 }
