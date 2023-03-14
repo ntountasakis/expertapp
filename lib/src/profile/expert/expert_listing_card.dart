20969d9c18c84d10fdad6ce031540e5f3e0b1a0b
Nicholas Tountasakis <nicholas.tountasakis@gmail.com>
2023-03-13 20:11:34 -0500

    EA-14 Enriched expert preview card

---
+++ lib/src/profile/expert/expert_listing_card.dart
@@ -0,0 +1,84 @@
+import 'package:expertapp/src/firebase/firestore/document_models/document_wrapper.dart';
+import 'package:expertapp/src/firebase/firestore/document_models/expert_rate.dart';
+import 'package:expertapp/src/firebase/firestore/document_models/public_expert_info.dart';
+import 'package:expertapp/src/navigation/routes.dart';
+import 'package:expertapp/src/profile/expert/expert_rating.dart';
+import 'package:expertapp/src/profile/profile_picture.dart';
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+class ExpertListingCard extends StatelessWidget {
+  final DocumentWrapper<PublicExpertInfo> _publicExpertInfo;
+
+  const ExpertListingCard(this._publicExpertInfo);
+
+  Widget buildLeading(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        context.pushNamed(Routes.UV_EXPERT_PROFILE_PAGE,
+            params: {Routes.EXPERT_ID_PARAM: _publicExpertInfo.documentId});
+      },
+      child: SizedBox(
+        width: 50,
+        child: ProfilePicture(_publicExpertInfo.documentType.profilePicUrl),
+      ),
+    );
+  }
+
+  Widget buildTitle() {
+    return Column(
+      children: [
+        Text(
+          _publicExpertInfo.documentType.shortName(),
+        ),
+        Row(
+          children: [
+            buildStarRating(_publicExpertInfo, 16),
+            SizedBox(width: 5),
+            buildTextRating(_publicExpertInfo, 16),
+          ],
+        ),
+      ],
+      crossAxisAlignment: CrossAxisAlignment.start,
+    );
+  }
+
+  Widget buildTrailing() {
+    final TextStyle rateStyle =
+        TextStyle(fontSize: 14, fontWeight: FontWeight.w500);
+    return FutureBuilder(
+        future: ExpertRate.get(_publicExpertInfo.documentId),
+        builder: (BuildContext context,
+            AsyncSnapshot<DocumentWrapper<ExpertRate>?> snapshot) {
+          if (snapshot.hasData) {
+            final rate = snapshot.data as DocumentWrapper<ExpertRate>;
+            return Column(
+              children: [
+                Text(
+                  "Start fee: " + rate.documentType.formattedStartCallFee(),
+                  style: rateStyle,
+                ),
+                Text(
+                  "Per minute fee: " +
+                      rate.documentType.formattedPerMinuteFee(),
+                  style: rateStyle,
+                ),
+              ],
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            );
+          }
+          return SizedBox();
+        });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        leading: buildLeading(context),
+        title: buildTitle(),
+        trailing: buildTrailing(),
+      ),
+    );
+  }
+}
---
+++ lib/src/profile/expert/expert_rating.dart
@@ -0,0 +1,29 @@
+import 'package:expertapp/src/firebase/firestore/document_models/document_wrapper.dart';
+import 'package:expertapp/src/firebase/firestore/document_models/public_expert_info.dart';
+import 'package:expertapp/src/profile/text_rating.dart';
+import 'package:flutter/material.dart';
+
+import '../star_rating.dart';
+
+Widget buildStarRating(
+    DocumentWrapper<PublicExpertInfo> publicExpertInfo, double size) {
+  return StarRating(
+      publicExpertInfo.documentType.getAverageReviewRating(), size);
+}
+
+Widget buildTextRating(
+    DocumentWrapper<PublicExpertInfo> publicExpertInfo, double size) {
+  return TextRating(
+      publicExpertInfo.documentType.getAverageReviewRating(), size);
+}
+
+Widget buildExpertProfileRating(
+    DocumentWrapper<PublicExpertInfo> publicExpertInfo) {
+  return Row(
+    children: [
+      Flexible(flex: 20, child: buildStarRating(publicExpertInfo, 25)),
+      Spacer(flex: 1),
+      Flexible(flex: 20, child: buildTextRating(publicExpertInfo, 18))
+    ],
+  );
+}
