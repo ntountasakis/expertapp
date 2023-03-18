58a45a77b94e19ace398e201d403fefe6440bb69
Nicholas Tountasakis <nicholas.tountasakis@gmail.com>
2023-03-18 14:24:14 -0500

    EA-4 Show dialog on success/fail account deletion

--- server/functions/src/endpoints/delete_user.ts
+++ server/functions/src/endpoints/delete_user.ts
@@ -9,18 +9,29 @@ export const deleteUser = functions.https.onCall(async (data, context) => {
     if (context.auth == null) {
         throw new Error("Cannot call by unauthorized users");
     }
-    const uid = context.auth.uid;
-    const privateUserInfo: PrivateUserInfo = await admin.firestore().runTransaction(async (transaction) => {
-        const privateUserInfo: PrivateUserInfo = await getPrivateUserDocument({ transaction: transaction, uid: uid });
-        transaction.delete(getPublicUserDocumentRef({ uid: uid }));
-        transaction.delete(getPrivateUserDocumentRef({ uid: uid }));
-        transaction.delete(getPublicExpertInfoDocumentRef({ uid: uid }));
-        transaction.delete(getExpertRateDocumentRef({ expertUid: uid }));
-        return privateUserInfo;
-    });
-
-    await deleteStripeCustomer({ stripe: StripeProvider.STRIPE, customerId: privateUserInfo.stripeCustomerId });
-    await admin.auth().deleteUser(uid);
-
     //todo: prompt user to delete stripe connected account before we proceed
+    try {
+        const uid = context.auth.uid;
+        const privateUserInfo: PrivateUserInfo = await admin.firestore().runTransaction(async (transaction) => {
+            const privateUserInfo: PrivateUserInfo = await getPrivateUserDocument({ transaction: transaction, uid: uid });
+            transaction.delete(getPublicUserDocumentRef({ uid: uid }));
+            transaction.delete(getPrivateUserDocumentRef({ uid: uid }));
+            transaction.delete(getPublicExpertInfoDocumentRef({ uid: uid }));
+            transaction.delete(getExpertRateDocumentRef({ expertUid: uid }));
+            return privateUserInfo;
+        });
+
+        await deleteStripeCustomer({ stripe: StripeProvider.STRIPE, customerId: privateUserInfo.stripeCustomerId });
+        await admin.auth().deleteUser(uid);
+    } catch (e) {
+        console.error(e);
+        return {
+            success: false,
+            message: "Please contact customer service. We are experiencing technical difficulties",
+        };
+    }
+    return {
+        success: true,
+        message: "Your account was deleted successfully",
+    };
 });
