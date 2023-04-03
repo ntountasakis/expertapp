b4d742c4f5d987a372885e13193d525242189841
Nicholas Tountasakis <nicholas.tountasakis@gmail.com>
2023-04-03 17:55:25 -0500

    EA-43 allow user to delete account if never fully created user account

--- server/functions/src/endpoints/delete_user.ts
+++ server/functions/src/endpoints/delete_user.ts
@@ -1,6 +1,6 @@
 import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
-import { getExpertRateDocumentRef, getPrivateUserDocument, getPrivateUserDocumentRef, getPublicExpertInfoDocumentRef, getPublicUserDocumentRef } from "../../../shared/src/firebase/firestore/document_fetchers/fetchers";
+import { getExpertRateDocumentRef, getPrivateUserDocumentRef, getPublicExpertInfoDocumentRef, getPublicUserDocumentRef } from "../../../shared/src/firebase/firestore/document_fetchers/fetchers";
 import { PrivateUserInfo } from "../../../shared/src/firebase/firestore/models/private_user_info";
 import { deleteStripeConnectedAccount, deleteStripeCustomer } from "../../../shared/src/stripe/util";
 import { StripeProvider } from "../../../shared/src/stripe/stripe_provider";
@@ -12,27 +12,29 @@ export const deleteUser = functions.https.onCall(async (data, context) => {
     }
     try {
         const uid = context.auth.uid;
-        const privateUserInfo: PrivateUserInfo = await admin.firestore().runTransaction(async (transaction) => {
-            return await getPrivateUserDocument({ transaction: transaction, uid: uid });
+        const privateUserInfo: PrivateUserInfo | null = await admin.firestore().runTransaction(async (transaction) => {
+            const doc = await transaction.get(getPrivateUserDocumentRef({ uid: uid }));
+            return doc.exists ? doc.data() as PrivateUserInfo : null;
         });
-
-        if (privateUserInfo.stripeConnectedId != null && privateUserInfo.stripeConnectedId !== "") {
-            const [hasZeroBalance, nonZeroMessage] = await allBalancesZeroStripeConnectedAccount({ stripe: StripeProvider.STRIPE, connectedAccountId: privateUserInfo.stripeConnectedId });
-            if (!hasZeroBalance) {
-                return {
-                    success: false,
-                    message: "Cannot delete account yet, all balances in your Stripe dashboard must be zero. " + nonZeroMessage,
-                };
+        if (privateUserInfo != null) {
+            if (privateUserInfo.stripeConnectedId != null && privateUserInfo.stripeConnectedId !== "") {
+                const [hasZeroBalance, nonZeroMessage] = await allBalancesZeroStripeConnectedAccount({ stripe: StripeProvider.STRIPE, connectedAccountId: privateUserInfo.stripeConnectedId });
+                if (!hasZeroBalance) {
+                    return {
+                        success: false,
+                        message: "Cannot delete account yet, all balances in your Stripe dashboard must be zero. " + nonZeroMessage,
+                    };
+                }
+                await deleteStripeConnectedAccount({ stripe: StripeProvider.STRIPE, connectedAccountId: privateUserInfo.stripeConnectedId });
             }
-            await deleteStripeConnectedAccount({ stripe: StripeProvider.STRIPE, connectedAccountId: privateUserInfo.stripeConnectedId });
+            await deleteStripeCustomer({ stripe: StripeProvider.STRIPE, customerId: privateUserInfo.stripeCustomerId });
+            await admin.firestore().runTransaction(async (transaction) => {
+                transaction.delete(getPublicUserDocumentRef({ uid: uid }));
+                transaction.delete(getPrivateUserDocumentRef({ uid: uid }));
+                transaction.delete(getPublicExpertInfoDocumentRef({ uid: uid }));
+                transaction.delete(getExpertRateDocumentRef({ expertUid: uid }));
+            });
         }
-        await deleteStripeCustomer({ stripe: StripeProvider.STRIPE, customerId: privateUserInfo.stripeCustomerId });
-        await admin.firestore().runTransaction(async (transaction) => {
-            transaction.delete(getPublicUserDocumentRef({ uid: uid }));
-            transaction.delete(getPrivateUserDocumentRef({ uid: uid }));
-            transaction.delete(getPublicExpertInfoDocumentRef({ uid: uid }));
-            transaction.delete(getExpertRateDocumentRef({ expertUid: uid }));
-        });
         await admin.auth().deleteUser(uid);
     } catch (e) {
         console.error(e);
