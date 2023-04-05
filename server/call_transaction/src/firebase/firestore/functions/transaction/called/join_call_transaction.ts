d22f5023311fcbfb1066a5374ce1d2c5239d1fda
Nicholas Tountasakis <nicholas.tountasakis@gmail.com>
2023-04-05 10:34:10 -0500

    EA-49 Audit call server console.errors, convert to throws so that client is notified of issue

--- server/call_transaction/src/firebase/firestore/functions/transaction/called/join_call_transaction.ts
+++ server/call_transaction/src/firebase/firestore/functions/transaction/called/join_call_transaction.ts
@@ -1,34 +1,31 @@
 import * as admin from "firebase-admin";
-import {getUtcMsSinceEpoch, isStringDefined} from "../../../../../../../shared/src/general/utils";
-import {getCallTransactionDocument, getCallTransactionDocumentRef, getPrivateUserDocument, getPublicExpertInfo, getPublicExpertInfoDocumentRef} from "../../../../../../../shared/src/firebase/firestore/document_fetchers/fetchers";
-import {CallTransaction} from "../../../../../../../shared/src/firebase/firestore/models/call_transaction";
-import {PrivateUserInfo} from "../../../../../../../shared/src/firebase/firestore/models/private_user_info";
-import {ClientCallJoinRequest} from "../../../../../protos/call_transaction_package/ClientCallJoinRequest";
-import {PublicExpertInfo} from "../../../../../../../shared/src/firebase/firestore/models/public_expert_info";
+import { getUtcMsSinceEpoch, isStringDefined } from "../../../../../../../shared/src/general/utils";
+import { getCallTransactionDocument, getCallTransactionDocumentRef, getPrivateUserDocument, getPublicExpertInfo, getPublicExpertInfoDocumentRef } from "../../../../../../../shared/src/firebase/firestore/document_fetchers/fetchers";
+import { CallTransaction } from "../../../../../../../shared/src/firebase/firestore/models/call_transaction";
+import { PrivateUserInfo } from "../../../../../../../shared/src/firebase/firestore/models/private_user_info";
+import { ClientCallJoinRequest } from "../../../../../protos/call_transaction_package/ClientCallJoinRequest";
+import { PublicExpertInfo } from "../../../../../../../shared/src/firebase/firestore/models/public_expert_info";
 
-export const joinCallTransaction = async ({request}: {request: ClientCallJoinRequest}):
-Promise<[boolean, CallTransaction]> => {
+export const joinCallTransaction = async ({ request }: { request: ClientCallJoinRequest }):
+  Promise<[boolean, CallTransaction]> => {
   if (request.callTransactionId == null) {
     throw new Error("ClientCallJoinRequest has null fields");
   }
   const transactionId = request.callTransactionId;
   return await admin.firestore().runTransaction(async (transaction) => {
     const callTransaction: CallTransaction = await getCallTransactionDocument(
-        {transaction: transaction, transactionId: transactionId});
-    const userInfo: PrivateUserInfo = await getPrivateUserDocument({transaction: transaction, uid: callTransaction.calledUid});
-    const publicExpertInfo: PublicExpertInfo = await getPublicExpertInfo({transaction: transaction, uid: callTransaction.calledUid});
+      { transaction: transaction, transactionId: transactionId });
+    const userInfo: PrivateUserInfo = await getPrivateUserDocument({ transaction: transaction, uid: callTransaction.calledUid });
+    const publicExpertInfo: PublicExpertInfo = await getPublicExpertInfo({ transaction: transaction, uid: callTransaction.calledUid });
 
     if (!isStringDefined(userInfo.stripeConnectedId)) {
-      console.error(`Uid ${callTransaction.calledUid} has no connected stripe account. Cannot join call`);
-      return [false, callTransaction];
+      throw new Error(`Uid ${callTransaction.calledUid} has no connected stripe account. Cannot join call`);
     }
     if (callTransaction.callerFinishedTransaction) {
-      console.error(`CallTransaction ${transactionId} has already ended. Cannot join call`);
-      return [false, callTransaction];
+      throw new Error(`CallTransaction ${transactionId} has already ended. Cannot join call`);
     }
     if (publicExpertInfo.inCall) {
-      console.error(`Uid ${callTransaction.calledUid} is in another call. Cannot join another`);
-      return [false, callTransaction];
+      throw new Error(`Uid ${callTransaction.calledUid} is in another call. Cannot join another`);
     }
 
     let errorMessage = `Call Transaction ID: ${request.callTransactionId} `;
@@ -41,11 +38,11 @@ Promise<[boolean, CallTransaction]> => {
 
     callTransaction.calledHasJoined = true;
     callTransaction.calledJoinTimeUtcMs = getUtcMsSinceEpoch();
-    transaction.update(getCallTransactionDocumentRef({transactionId: transactionId}), {
+    transaction.update(getCallTransactionDocumentRef({ transactionId: transactionId }), {
       "calledHasJoined": callTransaction.calledHasJoined,
       "calledJoinTimeUtcMs": callTransaction.calledJoinTimeUtcMs,
     });
-    transaction.update(getPublicExpertInfoDocumentRef({uid: callTransaction.calledUid}), {
+    transaction.update(getPublicExpertInfoDocumentRef({ uid: callTransaction.calledUid }), {
       "inCall": true,
     });
     return [true, callTransaction];
