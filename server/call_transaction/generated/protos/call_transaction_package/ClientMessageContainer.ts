df7c2ab7dc991d4108737618f81cc4e3de707f6c
Nick Tountasakis <ntountas@Nicks-MacBook-Air.local>
2023-05-25 10:28:23 -0500

    ios build fixes, rev flutter

--- server/call_transaction/generated/protos/call_transaction_package/ClientMessageContainer.ts
+++ server/call_transaction/generated/protos/call_transaction_package/ClientMessageContainer.ts
@@ -1,4 +1,4 @@
-// Original file: /home/ntountas/Code/expertapp/protos/call_transaction.proto
+// Original file: /Users/ntountas/Code/expertapp/protos/call_transaction.proto
 
 import type { ClientCallInitiateRequest as _call_transaction_package_ClientCallInitiateRequest, ClientCallInitiateRequest__Output as _call_transaction_package_ClientCallInitiateRequest__Output } from '../call_transaction_package/ClientCallInitiateRequest';
 import type { ClientCallJoinRequest as _call_transaction_package_ClientCallJoinRequest, ClientCallJoinRequest__Output as _call_transaction_package_ClientCallJoinRequest__Output } from '../call_transaction_package/ClientCallJoinRequest';
