6a43f44e8adb265e3bfcc886011216620bed147b
Nick Tountasakis <ntountas@Nicks-MacBook-Air.local>
2023-06-06 11:31:10 -0500

    EA-94 fixes for apple app store deployment

--- lib/src/environment/environment_config.dart
+++ lib/src/environment/environment_config.dart
@@ -2,7 +2,7 @@ import 'package:expertapp/src/environment/debug.dart';
 import 'package:expertapp/src/environment/prod.dart';
 
 abstract class EnvironmentConfig {
-  static const bool IS_PROD = false;
+  static const bool IS_PROD = true;
 
   String rpcServerHostname();
   int rpcServerPort();
